3623b51a42687231af7a0fecf476543f17270f5b
yaowen <yaowen369@163.com>
2017-08-13 23:24:19 +0800

    init dictionary

---
+++ data_structures/dictionary/Dictionary.js
@@ -0,0 +1,49 @@
+/**
+ * Created by yw on 17/8/13.
+ */
+
+
+function Dictionary() {
+    var items = {};
+    
+    //向字典中 添加新元素
+    this.set = function (key, value) {
+        
+    };
+
+    //通过使用健值，来从字典中移除对应的数据值
+    this.remove = function remove(key) {
+        
+    };
+
+    //该健值是否存在该字典中
+    this.has = function (key) {
+
+    };
+
+
+    //查找健值对应的元素并返回,不存在则返回null
+    this.get = function (key) {
+
+    };
+
+    //将字段中所有的元素全部清空
+    this.clear = function () {
+
+    };
+
+    //返回字典中所包含的元素的数量， 等同于数组的length
+    this.size = function () {
+
+    };
+
+    //将字典所包含的所有键名 以 数组形式返回
+    this.keys = function () {
+
+    };
+
+    // 将字典中所包含的值 以数组形式返回 
+    this.values = function () {
+
+    };
+}
