9330ad791718181128baa16be4a904a1960d347d
yaowen <yaowen369@163.com>
2017-09-21 23:22:03 +0800

    二叉搜索树实现了一部分

--- data_structures/tree/BinarySearchTree.js
+++ data_structures/tree/BinarySearchTree.js
@@ -15,14 +15,61 @@ function BinarySearchTree() {
         this.right = null;
     };
 
+    var root = null;
+
     //向树中插入一个新的键
     this.insert = function (key) {
+      if (root == null){
+          root = new Node(key);
+      }else {
+          insertNode(root, key);
+      }
+    };
 
+    var insertNode = function (parentNode , key) {
+        if (key < parentNode.key){
+            //插在左侧
+            if (parentNode.left == null){
+                parentNode.left = new Node(key);
+            }else {
+                insertNode(parentNode.left, key);
+            }
+        }else {  //插在右侧
+            if (parentNode.right == null){
+                parentNode.right = new Node(key);
+            }else {
+                insertNode(parentNode.right, key);
+            }
+        }
     };
 
     // 在树中查找一个键，如果节点存在，则返回true； 否则返回false
     this.search = function (key) {
+      if (root == null){
+          return false;
+      } else {
+          return searchNode(root, key);
+      }
+    };
 
+    var searchNode = function (parentNode, key) {
+        if (key === parentNode.key){
+            return true;
+        }else if (key < parentNode.key){
+            //应该搜索左边
+            if (parentNode.left == null){
+                return false;
+            }else {
+                return searchNode(parentNode.left, key);
+            }
+        } else {
+            //应该搜索右边
+            if (parentNode.right == null){
+                return false;
+            }else {
+                return searchNode(parentNode.right, key);
+            }
+        }
     };
 
     //通过中序遍历方式 遍历所有节点
@@ -41,15 +88,46 @@ function BinarySearchTree() {
 
     };
 
-    //返回树中最小的值/键
+    //返回树中最小的值/键, 或则null，（root为null的情况下）
     this.min = function () {
-
+        if (root == null){
+            return null;
+        }  else {
+            if (root.left == null){
+                return root.key;
+            }else {
+                return minInTree(root.left);
+            }
+        }
     };
 
+    var minInTree = function (parentNode) {
+        if (parentNode.left == null){
+            return parentNode.key;
+        }else {
+            return minInTree(parentNode.left);
+        }
+    };
 
-    //返回树种最大的值/键
+    //返回树中最大的值/键,或者 null。(root为null时)
     this.max = function () {
+        if (root == null){
+            return null;
+        }else {
+            if (root.right == null){
+                return root.key;
+            }else {
+                maxInTree(root.right);
+            }
+        }
+    };
 
+    var maxInTree = function (parentNode) {
+        if (parentNode.right == null){
+            return parentNode.key;
+        }else {
+            maxInTree(parentNode.right);
+        }
     };
 
     //从树中 移除某个键
